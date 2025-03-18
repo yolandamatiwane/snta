b221b53fd308ef1c0900b85793f09afbc3241d64
yolandamatiwane <mbalentle.matiwane21@gmail.com>
2025-03-19 00:07:18 +0200

    gn

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,29 @@
 <template>
   <div class="home">
-    <p v-for="data in getData" :key="data.siteId">
-      {{data.price}}
-    </p>
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Category</th>
+          <th scope="col">Full Name</th>
+          <th scope="col">Price</th>
+          <th scope="col">Move</th>
+          <th scope="col">Pmove</th>
+          <th scope="col">Date & Time</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="data in getData" :key="data.siteId">
+          <td>{{ data.categoryName }}</td>
+          <td>{{ data.fullName }}</td>
+          <td>{{ data.price }}</td>
+          <td>{{ data.move }}</td>
+          <td>{{ data.pmove }}</td>
+          <td>{{ data.datetime }}</td>
+        </tr>
+      </tbody>
+    </table>
+
   </div>
 
 </template>
